1357a3501d3070c2c85e91ffb80a8ef4881d8a56
ninthshark <kiattisak.dujda@gmail.com>
2017-09-23 21:13:11 +0100

    Emit messages

--- server/server.js
+++ server/server.js
@@ -1,16 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     res.render('index.html');
-// });
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-app.listen(port, () => {
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
